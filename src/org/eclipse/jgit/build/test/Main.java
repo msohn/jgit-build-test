519429d7845eca38c798c03a3905af0ae2222532
Matthias Sohn <matthias.sohn@sap.com>
2020-06-01 01:11:43 +0200

    Update to jgit 5.8.0-SNAPSHOT

--- src/org/eclipse/jgit/build/test/Main.java
+++ src/org/eclipse/jgit/build/test/Main.java
@@ -4,8 +4,10 @@ import java.io.IOException;
 import java.nio.file.Files;
 
 import org.eclipse.jgit.lfs.Lfs;
+import org.eclipse.jgit.lib.GpgSigner;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.eclipse.jgit.transport.SshSessionFactory;
 
 public class Main {
 
@@ -13,6 +15,10 @@ public class Main {
     Repository db = FileRepositoryBuilder.create(Files.createTempDirectory("jgit-build-test").toFile());
     Lfs lfs = new Lfs(db);
     System.out.println(lfs.getLfsObjDir());
+    GpgSigner signer = GpgSigner.getDefault();
+    System.out.println(signer.getClass().getCanonicalName());
+    SshSessionFactory sshFactory = SshSessionFactory.getInstance();
+    System.out.println(sshFactory.getClass().getCanonicalName());
   }
 
 }
